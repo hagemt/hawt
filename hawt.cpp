002b61615984d4a0769dd8af046f9e255daa5fe5
Tor E Hagemann <hagemt@rpi.edu>
2012-07-19 16:24:11 -0500

    Implemented windows background switching

--- hawt.cpp
+++ hawt.cpp
@@ -21,26 +21,27 @@ main(int argc, char *argv[])
 	if (QSystemTrayIcon::isSystemTrayAvailable()) {
 		context_menu = new QMenu(&window);
 		change_action = new QAction(QObject::tr("Shuffle!"), context_menu);
-		show_action = new QAction(QObject::tr("%1 Settings").arg(CLIENT_NAME), context_menu);
+		show_action = new QAction(QObject::tr("%1 Settings").arg(PROGRAM_NAME), context_menu);
 		show_action->setCheckable(true);
 		show_action->setChecked(false);
-		QObject::connect(show_action, SIGNAL(toggled(bool)), &window, SLOT(setVisible(bool)));
+		QObject::connect(show_action, SIGNAL(triggered(bool)), &window, SLOT(setVisible(bool)));
 		context_menu->addAction(change_action);
 		context_menu->addSeparator();
 		context_menu->addAction(show_action);
 		tray_icon = new QSystemTrayIcon(QIcon(":/images/tray-icon.png"), context_menu);
 		tray_icon->setContextMenu(context_menu);
-		tray_icon->setToolTip(QObject::tr("%1 >;3").arg(CLIENT_NAME));
+		tray_icon->setToolTip(QObject::tr("%1 >;3").arg(PROGRAM_NAME));
 		tray_icon->show();
-	// Or just show the settings window
 	} else if (QMessageBox::question(&window,
 					QObject::tr("Oh noes! D="),
-					QObject::tr("%1 can't find the system tray; launch anyway?").arg(CLIENT_NAME),
+					QObject::tr("%1 can't find the system tray; launch anyway?").arg(PROGRAM_NAME),
 					QMessageBox::Yes | QMessageBox::Close,
 					QMessageBox::Close)
 				!= QMessageBox::Close) {
+		// Or just show the settings window
 		window.show();
 	} else {
+		// Alternative is failure
 		window.close();
 	}
 	// Run the event loop
--- hawt.h
+++ hawt.h
@@ -1,8 +1,16 @@
-#define CLIENT_NAME "HAWT"
+#ifndef HAWT_H
+#define HAWT_H
+
+#define PROGRAM_NAME "HAWT"
+#define COMPANY_NAME "onetooneandon.to"
+
+#ifdef Q_WS_WIN
+#include <windows.h>
+#include <QSettings>
+#endif
+
+#include <QDir>
 #include <QString>
 void set_wallpaper(QString);
-#ifdef WIN32
-#include "hawt_windows.h"
-#else
-#include "hawt_gnome.h"
-#endif
+
+#endif /* HAWT_H */
