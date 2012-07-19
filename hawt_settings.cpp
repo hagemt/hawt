84633b54e5bff9da4b3532c77096e01889ba8d60
Tor E Hagemann <hagemt@rpi.edu>
2012-07-19 16:23:04 -0500

    Added tab-order, buddies, and more base functions
    Also fixed tabbing

--- hawt_settings.cpp
+++ hawt_settings.cpp
@@ -1,26 +1,47 @@
 #include "hawt_settings.h"
 #include "ui_hawt_settings.h"
 
+#include "hawt.h"
+
 SettingsInterface::SettingsInterface(QWidget *parent) :
-    QFrame(parent),
-    ui(new Ui::SettingsInterface)
+	QFrame(parent),
+	ui(new Ui::SettingsInterface),
+	settings(QSettings::IniFormat, QSettings::UserScope, COMPANY_NAME, PROGRAM_NAME)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+	// attach reset button to default settings
+	load_settings();
 }
 
 SettingsInterface::~SettingsInterface()
 {
-    delete ui;
+	save_settings();
+	delete ui;
 }
 
 void SettingsInterface::changeEvent(QEvent *e)
 {
-    QFrame::changeEvent(e);
-    switch (e->type()) {
-    case QEvent::LanguageChange:
-        ui->retranslateUi(this);
-        break;
-    default:
-        break;
-    }
+	QFrame::changeEvent(e);
+	switch (e->type()) {
+	case QEvent::LanguageChange:
+		ui->retranslateUi(this);
+		break;
+	default:
+		break;
+	}
+}
+
+void SettingsInterface::save_settings()
+{
+	emit settings_changed();
+}
+
+void SettingsInterface::load_settings()
+{
+	
+}
+
+void SettingsInterface::reset_settings()
+{
+
 }
--- hawt_settings.h
+++ hawt_settings.h
@@ -2,22 +2,35 @@
 #define SETTINGSINTERFACE_H
 
 #include <QFrame>
+#include <QSettings>
+
+#include "hawt.h"
 
 namespace Ui {
-    class SettingsInterface;
+	class SettingsInterface;
 }
 
 class SettingsInterface : public QFrame {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    SettingsInterface(QWidget *parent = 0);
-    ~SettingsInterface();
+	SettingsInterface(QWidget *parent = 0);
+	~SettingsInterface();
+
+public slots:
+	void save_settings();
+	void reset_settings();
+
+signals:
+	void settings_changed();
 
 protected:
-    void changeEvent(QEvent *e);
+	void changeEvent(QEvent *e);
 
 private:
-    Ui::SettingsInterface *ui;
+	Ui::SettingsInterface *ui;
+	QSettings settings;
+
+	void load_settings();
 };
 
 #endif // SETTINGSINTERFACE_H
